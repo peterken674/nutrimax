3095256546fd55d211a6888e86999fb0490e1110
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-25 00:20:04 +0300

    Change colors on info page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from ..models import Total
 from flask import render_template, flash
 from flask.globals import request
 from flask_login.utils import login_required
@@ -19,12 +20,43 @@ def index():
 def info():
     '''View the search page.
     '''
-    foods = get_details("3 chapatis and bean")
+    foods = get_details("3 burgers and fries and orange juice")
+
+    nf_total_fat = 0.0
+    nf_saturated_fat = 0.0
+    nf_cholesterol = 0.0
+    nf_sodium = 0.0
+    nf_total_carbohydrates = 0.0
+    nf_dietary_fiber = 0.0
+    nf_sugars = 0.0
+    nf_proteins = 0.0
+    nf_potassium = 0.0
 
 
+    for food in foods:
+        if food.nf_total_fat:
+            nf_total_fat += food.nf_total_fat
+        if food.nf_saturated_fat:    
+            nf_saturated_fat += food.nf_saturated_fat
+        if food.nf_cholesterol: 
+            nf_cholesterol += food.nf_cholesterol 
+        if food.nf_sodium:
+            nf_sodium += food.nf_sodium 
+        if food.nf_total_carbohydrates:
+            nf_total_carbohydrates += food.nf_total_carbohydrates
+        if food.nf_dietary_fiber:
+            nf_dietary_fiber += food.nf_dietary_fiber
+        if food.nf_sugars:
+            nf_sugars += food.nf_sugars
+        if food.nf_proteins:
+            nf_proteins += food.nf_proteins
+        if food.nf_potassium:
+            nf_potassium += food.nf_potassium
+
+        totals = Total(nf_total_fat, nf_saturated_fat, nf_cholesterol, nf_sodium, nf_total_carbohydrates, nf_dietary_fiber, nf_sugars, nf_proteins, nf_potassium)
 
     title = 'Info | Nutrimax'
-    return render_template('food_info.html', title=title, foods=foods)
+    return render_template('food_info.html', title=title, foods=foods, totals=totals)
 
 @main.route('/about', methods=['GET', 'POST'])
 def about():
