d0961359b1bc99d04b3fd6047a86ed0de36b5cbb
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-24 22:50:56 +0300

    Final merge commit.

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class ProdConfig(Config):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:0716501927@localhost/project_week_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peterken:pa55@localhost/project_week_test'
 
 class DevConfig(Config):
     '''
@@ -38,7 +38,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:0716501927@localhost/project_week'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peterken:pa55@localhost/project_week'
     DEBUG = True
 
 config_options = {
